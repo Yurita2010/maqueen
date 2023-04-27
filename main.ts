93306793a211f3e547f3a2cca9dbfe96d310f7fa
Yurita2010 <123651093+Yurita2010@users.noreply.github.com>
2023-04-27 16:05:13 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
+let ir_waarde = 0
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 49)
-    control.waitMicros(4)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 49)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 49)
-    control.waitMicros(4)
+    basic.showNumber(ir_waarde)
+})
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
 })
